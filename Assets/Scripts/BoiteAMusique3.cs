c3770ec16309aebbb05831613246d6959b007f00
Sephun <pseloushodges@gmail.com>
2025-12-16 17:54:59 +0100

    boite a musique fr

--- Assets/Scripts/BoiteAMusique3.cs
+++ Assets/Scripts/BoiteAMusique3.cs
@@ -4,26 +4,20 @@ using UnityEngine;
 
 public class BoiteAMusique3 : MonoBehaviour
 {
-    public GameObject OpenMusicBox3;
-    public GameObject ClosedMusicBox3;
-    public Vector3 ClosedBoxPos3;
+    public OpenMusicBox openMusicBox;
+    public bool IsBox3;
 
     public void OnTriggerEnter(Collider other)
     {
+
         if (other.tag == "BoiteMusique3")
         {
-            ClosedBoxPos3 = ClosedMusicBox3.transform.position;
-            StartCoroutine(MusicBox3());
+            IsBox3 = true;
         }
-
-    }
-
-    public IEnumerator MusicBox3()
-    {
-        yield return new WaitForSeconds(2f);
-        ClosedMusicBox3.gameObject.SetActive(false);
-        OpenMusicBox3.gameObject.SetActive(true);
-        OpenMusicBox3.transform.position = ClosedBoxPos3;
+        else
+        {
+            IsBox3 = false;
+        }
     }
 
 }
--- Assets/Scripts/BoiteAMusique4.cs
+++ Assets/Scripts/BoiteAMusique4.cs
@@ -4,26 +4,20 @@ using UnityEngine;
 
 public class BoiteAMusique4 : MonoBehaviour
 {
-    public GameObject OpenMusicBox4;
-    public GameObject ClosedMusicBox4;
-    public Vector3 ClosedBoxPos4;
+
+    public OpenMusicBox openMusicBox;
+    public bool IsBox4;
 
     public void OnTriggerEnter(Collider other)
     {
 
         if (other.tag == "BoiteMusique4")
         {
-            ClosedBoxPos4 = ClosedMusicBox4.transform.position;
-            StartCoroutine(MusicBox4());
+           IsBox4 = true;
         }
-
+        else
+        {
+            IsBox4 = false;
+        }
     }
-    public IEnumerator MusicBox4()
-    {
-        yield return new WaitForSeconds(2f);
-        ClosedMusicBox4.gameObject.SetActive(false);
-        OpenMusicBox4.gameObject.SetActive(true);
-        OpenMusicBox4.transform.position = ClosedBoxPos4;
-    }
-
 }
